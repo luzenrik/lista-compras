1b88c7a549d3951a52ff6e34cb64d0be3034710d
luzenrik <luizhenriquers03@gmail.com>
2024-11-08 13:26:58 -0300

    def remover e carregar adicionada mas continua dadno erro

--- lista_compras.py
+++ lista_compras.py
@@ -4,6 +4,7 @@ import os
 #nome do arquivo que armazenará a lista de compras
 ARQUIVO_LISTA = 'lista_compras.txt'
 
+#funçao exibir lista
 def exibir_lista(lista_compras):
     """
     Exibe todos os itens da lista de compras.
@@ -12,19 +13,43 @@ def exibir_lista(lista_compras):
     for i, item in enumerate(lista_compras, start=1):
         print(f"{i}. {item}")
 
-
+#função add item na lista
 def adicionar_item():
     """
     Adiciona um novo item à lista de compras.
     """
     item = input("Digite o item para adicionar: ")
     lista_compras.append(item)
-    print(f"Item '{item}' adicionado com sucesso!")
+    print(f"Item '{item}' adicionado")
 
-#def remover_item():
+#função de removaer item da lista
+def remover_item():
+    """
+    Remove um item específico da lista de compras.
+    """
+    exibir_lista(lista_compras)
+    indice = int(input("Digite o número do item para remover: ")) - 1
+    if 0 <= indice < len(lista_compras):
+        item_removido = lista_compras.pop(indice)
+        print(f"Item '{item_removido}' removido")
+    else:
+        print("Índice inválido.")
     
 #def salvar_lista(): 
-          
+
+# função de mostrar lista
+def carregar_lista():
+    """
+    Carrega a lista de compras de um arquivo de texto.
+    """
+    lista_compras = []
+    if os.path.exists(ARQUIVO_LISTA):
+        with open(ARQUIVO_LISTA, 'r') as arquivo:
+            for linha in arquivo:
+                lista_compras.append(linha.strip())
+    return lista_compras
+
+# funcão principal em loop
 def main():
     lista_compras = carregar_lista()
 
